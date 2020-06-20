a029da1459dbfa47b11d1bdcca4930bb14fb942c
yangqinjiang <531553280@qq.com>
2020-06-20 14:02:25 +0800

    source

--- src/main/scala/com/atguigu/apitest/SourceTest.scala
+++ src/main/scala/com/atguigu/apitest/SourceTest.scala
@@ -1,6 +1,13 @@
 package com.atguigu.apitest
 
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+
+import scala.util.Random
 
 //温度传感器读数样例类=
 case class SensorReading(id:String, timestamp:Long,temperature:Double)
@@ -25,6 +32,62 @@ object SourceTest {
     //4,
     env.fromElements(1,2,3.0,"string").print("fromElements").setParallelism(1)
 
+    //5，从kafka源读取数据
+    //配置
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers","localhost:9092")
+    properties.setProperty("group.id","consumer-group")
+
+    properties.setProperty("key.deserializer",
+      "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("value.deserializer",
+      "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("auto.offset.reset", "latest")
+    //运行出错时，flink将偏移量状态恢复到kafka
+    //自动保证运行
+    val stream3 = env.addSource(new FlinkKafkaConsumer011[String]("sensor", new SimpleStringSchema(),properties))
+    stream3.print("source kafka steam3").setParallelism(1)
+
+
+    // 6 自定义source
+    val stream4 = env.addSource(new MySensorSource())
+    stream4.print("MySensorSource").setParallelism(1)
     env.execute("source test")
   }
+}
+//自定义source,随机生成数据
+class MySensorSource extends  SourceFunction[SensorReading]{
+
+  //标识变量，表示数据源是否还在正常运行
+  var running:Boolean = true
+  override def cancel(): Unit = {
+    running = false // 取消时， 设置为false
+  }
+
+  override def run(ctx: SourceFunction.SourceContext[SensorReading]): Unit = {
+    //初始化一个随机数发生器
+    val rand = new Random()
+
+    var curTemp = 1.to(10).map(
+      i => ("sensor_"+i , 65 + rand.nextGaussian() * 20)
+    )
+
+    while (running){
+      //更新温度值
+      curTemp = curTemp.map(
+        t => (t._1, t._2 + rand.nextGaussian())
+      )
+
+      //获取当前时间戳
+      var curTime = System.currentTimeMillis()
+
+      curTemp.foreach(
+        t => ctx.collect(SensorReading(t._1,curTime,t._2))
+      )
+      Thread.sleep(1000)//休眠x ms
+    }
+  }
+
+
+
 }
